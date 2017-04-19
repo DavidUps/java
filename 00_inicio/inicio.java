71613b238ab1c5a69d3eb35d5f887f6a1aa5aa3b
Santiago <santiagosieger.ciclos@gmail.com>
2017-04-19 12:44:36 +0200

    java

--- 00_inicio/inicio.java
+++ 00_inicio/inicio.java
@@ -5,7 +5,7 @@
 class Principal{
 
   public static void main(String[] args){
-  
+
       int a    = 5;
       String p = "David5";
       char c   = 'e';
---
+++ PruebaFernando/src/MiClase.java
@@ -0,0 +1,12 @@
+public class MiClase {
+
+	private int a = 2;
+	private String b;
+	
+	public MiClase(int a, String b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	
+}
